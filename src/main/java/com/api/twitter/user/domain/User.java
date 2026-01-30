04448ee3ba5d5caf1f28d52395cc04824a9f3c7f
filipemartinsdev <dev.filipemartins@gmail.com>
2026-01-30 20:45:52 -0300

    feat(user): implement self validation methods to User Entity

--- src/main/java/com/api/twitter/user/domain/User.java
+++ src/main/java/com/api/twitter/user/domain/User.java
@@ -1,6 +1,7 @@
 package com.api.twitter.user.domain;
 
 import com.api.twitter.common.model.UserRole;
+import com.api.twitter.user.application.exception.UserValidationException;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
@@ -36,4 +37,36 @@ public class User {
 
     @Enumerated(EnumType.STRING)
     private UserRole role;
+
+    // TODO: IMPROVE VALIDATION RESPONSE
+    public void validateUsername(){
+        final int MIN_LEN_USERNAME = 3;
+        final int MAX_LEN_USERNAME = 25;
+
+        if (
+                this.username != null ||
+                this.username.length() < MIN_LEN_USERNAME ||
+                this.username.length() > MAX_LEN_USERNAME
+        ){
+            throw new UserValidationException("Invalid username");
+        }
+    }
+
+    private void validateEmail(){
+        // TODO
+    }
+
+    // TODO: IMPROVE VALIDATION RESPONSE
+    public void validatePassword(){
+        final int MIN_LEN_PASSWORD = 5;
+        final int MAX_LEN_PASSWORD = 50;
+
+        if (
+                this.password != null ||
+                this.password.length() < MIN_LEN_PASSWORD ||
+                this.password.length() > MAX_LEN_PASSWORD
+        ){
+            throw new UserValidationException("Invalid password");
+        }
+    }
 }
