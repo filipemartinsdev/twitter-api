7e9f4787e5b1973d75ce93dc92b2f40e248567e3
filipemartinsdev <dev.filipemartins@gmail.com>
2026-02-18 19:46:48 -0300

    feat: configure spring cache

--- src/main/java/com/api/twitter/cache/CacheConfiguration.java
+++ src/main/java/com/api/twitter/cache/CacheConfiguration.java
@@ -1,7 +1,12 @@
 package com.api.twitter.cache;
 
+import org.springframework.boot.cache.autoconfigure.RedisCacheManagerBuilderCustomizer;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+
+import java.time.Duration;
 
 
 @Configuration
