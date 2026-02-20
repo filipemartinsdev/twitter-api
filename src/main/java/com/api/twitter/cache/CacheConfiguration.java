9c0139f74e83c54b995f4fb17c7162c0df8adf2c
filipemartinsdev <dev.filipemartins@gmail.com>
2026-02-20 11:15:11 -0300

    fix(cache): reconfigure cache to serialize successfully without JSON config

--- src/main/java/com/api/twitter/cache/CacheConfiguration.java
+++ src/main/java/com/api/twitter/cache/CacheConfiguration.java
@@ -1,29 +1,24 @@
 package com.api.twitter.cache;
 
-import org.springframework.boot.cache.autoconfigure.RedisCacheManagerBuilderCustomizer;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.serializer.GenericJacksonJsonRedisSerializer;
-import org.springframework.data.redis.serializer.RedisSerializationContext;
-import tools.jackson.databind.ObjectMapper;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 
 import java.time.Duration;
-
 
 @Configuration
 public class CacheConfiguration {
     @Bean
-    public RedisCacheManagerBuilderCustomizer redisCacheManagerBuilderCustomizer(ObjectMapper objectMapper) {
-        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
-                .disableCachingNullValues()
-                .serializeValuesWith(
-                        RedisSerializationContext.SerializationPair.fromSerializer(
-                                new GenericJacksonJsonRedisSerializer(objectMapper)
-                        )
-                );
+    public RedisCacheManager redisCacheManager(RedisConnectionFactory redisConnectionFactory){
+        return RedisCacheManager.create(redisConnectionFactory);
+    }
 
-        return builder -> builder.cacheDefaults(config);
+    @Bean
+    public RedisCacheConfiguration redisCacheConfiguration(){
+        return RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(60))
+                .disableCachingNullValues();
     }
 }
